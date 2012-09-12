84ec064ea118cbbcfbd3cb922651d50052c0a460
Brian Luft <brian@unbracketed.com>
2012-09-12 14:01:19 -0700

    cleanup and reorg code

--- mixer/mixer.ino
+++ mixer/mixer.ino
@@ -1,59 +1,62 @@
 /*
-dev-lights
+RGB-LED-mixer
  
- Ambient feedback indicators for CI or test suite-related events
- */
+Tools for mixing and tuning RGB LEDs 
+*/
 
-int level_R = 0;
-int level_G = 1;
-int level_B = 2;
 
-//int debug_led = 8;
-int indicator_R = 9;
-int indicator_G = 10;
-int indicator_B = 11;
+/*
+RGB level inputs
+*/
+const int level_R = 0;
+const int level_G = 1;
+const int level_B = 2;
+
+/*
+RGB output channels
+*/
+const int chan_R = 9;
+const int chan_G = 10;
+const int chan_B = 11;
 
 
 void setup() {                
-  //  pinMode(debug_led, OUTPUT);   
-  pinMode(indicator_R, OUTPUT);   
-  pinMode(indicator_G, OUTPUT);   
-  pinMode(indicator_B, OUTPUT);
-  //  pinMode(level_R, INPUT);
-  //  pinMode(level_G, INPUT);
-  //  pinMode(level_B, INPUT);
+
+  pinMode(chan_R, OUTPUT);   
+  pinMode(chan_G, OUTPUT);   
+  pinMode(chan_B, OUTPUT);
+
   // initialize serial communication at 9600 bits per second:
   Serial.begin(9600); 
 }
 
+//values to hold last input readings
 int oldr = 0;
 int oldg = 0;
 int oldb = 0;
 
+/*
+check an input and if the reading has changed since the
+last check, update the PWM output channel
+*/
+void check_channel(int input, int &last_val, int output){
+  int cur_val = map(analogRead(input), 0, 1023, 0, 255);
+  if (abs(cur_val - last_val) > 1){
+    last_val = cur_val;
+    Serial.println(cur_val);
+    analogWrite(output, cur_val);
+  }
+
+}
 
 void loop() {  
-  // read the input on analog pin 0:
-  int cur_R = map(analogRead(level_R), 0, 1023, 0, 255);
-  int cur_G = map(analogRead(level_G), 0, 1023, 0, 255);
-  int cur_B = map(analogRead(level_B), 0, 1023, 0, 255);  
-
-  if (abs(oldr - cur_R) > 1) {
-    oldr = cur_R;
-    Serial.println(String(cur_R) + " " + String(cur_G) + " " + String(cur_B));
-    analogWrite(indicator_R, cur_R);
-  }
-  if (abs(oldg - cur_G) > 1) {
-    oldg = cur_G;
-    Serial.println(String(cur_R) + " " + String(cur_G) + " " + String(cur_B));
-    analogWrite(indicator_G, cur_G);
-  }
-  if (abs(oldb - cur_B) > 1) {
-    oldb = cur_B;
-    Serial.println(String(cur_R) + " " + String(cur_G) + " " + String(cur_B));
-    analogWrite(indicator_B, cur_B);
-  }
+  //read input levels for RGB channels
+  check_channel(level_R, oldr, chan_R);
+  check_channel(level_G, oldb, chan_G);
+  check_channel(level_B, oldg, chan_B);
   delay(1);        // delay in between reads for stability
 }
+
 
 
 
